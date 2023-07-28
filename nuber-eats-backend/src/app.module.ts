d7c9dcb4ea8676f98131bf8373744b8bfe6b48cd
minsu.rob <minsu.rob@gmail.com>
2023-07-29 00:42:14 +0900

    typeorm local setting

--- nuber-eats-backend/src/app.module.ts
+++ nuber-eats-backend/src/app.module.ts
@@ -1,11 +1,24 @@
+/* eslint-disable */
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+  imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "1234",
+      database: "nuber-eats",
+      synchronize: true,
+      logging: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: true,
   }), RestaurantsModule],
