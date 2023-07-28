3d00c7e9a0729ad430524548f398aaacf5e77a2f
minsu.rob <minsu.rob@gmail.com>
2023-07-29 01:43:57 +0900

    typeorm 데코레이터 사용

--- nuber-eats-backend/src/restaurants/entities/restaurant.entity.ts
+++ nuber-eats-backend/src/restaurants/entities/restaurant.entity.ts
@@ -1,9 +1,19 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+/* eslint-disable */
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+    @PrimaryGeneratedColumn()
+    @Field(type => Number)
+    id: number
+
     @Field(type => String)
+    @Column()
     name: string;
+    
     @Field(type => Boolean, {nullable:true})
+    @Column()
     isGood?: boolean;
 }
