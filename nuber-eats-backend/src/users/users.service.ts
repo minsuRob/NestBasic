425cc0a27a1f12cf24b06b25ef52d2e4e31c0f9f
minsu.rob <minsu.rob@gmail.com>
2023-08-02 19:20:12 +0900

    5.4 정리
    - entity type을 enum으로 변경.
    - 기존 email 여부를 체크 한뒤에, email, pwd, 역할을 저장함

--- nuber-eats-backend/src/users/users.service.ts
+++ nuber-eats-backend/src/users/users.service.ts
@@ -3,10 +3,24 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { User } from "./entities/user.entity";
 import { Injectable } from '@nestjs/common';
+import { CreateAccountInput } from "./dtos/create-account-dto";
 
 @Injectable()
 export class usersService {
     constructor(
-        @InjectRepository(User) private readonly user: Repository<User>
+        @InjectRepository(User) private readonly users: Repository<User>
     ) {}
+
+    async createAccount({ email, password, role } : CreateAccountInput) {
+        try {
+            const exists = await this.users.findOneBy({ email });
+            if (exists) {
+                return;
+            }
+            await this.users.save(this.users.create({email, password, role}));
+            return true;
+        } catch (error) {
+            return;
+        }
+    }
 }
